d1bad04b18f25f06bb9aa399ad762652015fd596
Felipe Marini <felipemarini71188@gmail.com>
2021-03-02 17:50:29 -0300

    RPG Hroads finalizado

--- sprint-1-bd/desafios/hroads_01_DDL.sql
+++ sprint-1-bd/desafios/hroads_01_DDL.sql
@@ -1,43 +1,40 @@
+CREATE DATABASE Rpg_Hroads;
 
--- 1. Criar o banco de dados chamado SENAI_HROADS_MANHA/SENAI_HROADS_TARDE (conforme o período)
-CREATE DATABASE SENAI_HROADS_TARDE;
-
-USE SENAI_HROADS_TARDE;
+USE Rpg_Hroads;
 
 
--- 2. Criar as tabelas no banco de dados
-CREATE TABLE Classe
+CREATE TABLE Classes
 (
-	idClasse INT PRIMARY KEY IDENTITY
-	, Nome_da_Classe VARCHAR(200)
+	idClasse INT PRIMARY KEY IDENTITY, 
+	Nome_da_Classe VARCHAR(200)
 );
 
-CREATE TABLE Personagem
+CREATE TABLE Personagens
 (
-	idPersonagem INT PRIMARY KEY IDENTITY
-	, idClasse INT FOREIGN KEY REFERENCES Classe(idCLasse)
-	, Nome_do_Personagem VARCHAR(200) NOT NULL
-	, Vida_Maxima INT NOT NULL
-	, Mana_Maxima INT NOT NULL
-	, Data_de_Criacao DATE NOT NULL
-	, Data_de_Atualizacao DATE NOT NULL
+	idPersonagem INT PRIMARY KEY IDENTITY, 
+	idClasse INT FOREIGN KEY REFERENCES Classes(idCLasse),
+	Nome_do_Personagem VARCHAR(200) NOT NULL,
+	Vida_Maxima INT NOT NULL,
+	Mana_Maxima INT NOT NULL,
+	Data_de_Criacao DATETIME NOT NULL,
+	Data_de_Atualizacao DATETIME NOT NULL
 );
 
 CREATE TABLE Tipo_de_Habilidade
 (
-	idTipoHabilidade INT PRIMARY KEY IDENTITY
-	, Tipo_de_Habilidade VARCHAR(200)
+	idTipoHabilidade INT PRIMARY KEY IDENTITY, 
+	Tipo_de_Habilidade VARCHAR(200)
 );
 
-CREATE TABLE Habilidade
+CREATE TABLE Habilidades
 (
-	idHabilidade INT PRIMARY KEY IDENTITY
-	, idTipoHabilidade INT FOREIGN KEY REFERENCES Tipo_de_Habilidade(idTipoHabilidade)
-	, Nome_Habilidade VARCHAR(200)
+	idHabilidade INT PRIMARY KEY IDENTITY,
+	idTipoHabilidade INT FOREIGN KEY REFERENCES Tipo_de_Habilidade(idTipoHabilidade),
+	Nome_Habilidade VARCHAR(200)
 );
 
 CREATE TABLE Habilidades_da_Classe
 (
-	idClasse INT FOREIGN KEY REFERENCES Classe(idCLasse)
-	, idHabilidade INT FOREIGN KEY REFERENCES Habilidade(idHabilidade)
+	idClasse INT FOREIGN KEY REFERENCES Classes(idCLasse),
+	idHabilidade INT FOREIGN KEY REFERENCES Habilidades(idHabilidade)
 );
--- sprint-1-bd/desafios/hroads_03_DQL.sql
+++ sprint-1-bd/desafios/hroads_03_DQL.sql
@@ -1,61 +1,66 @@
-USE SENAI_HROADS_TARDE;
+USE Rpg_Hroads;
 
--- 6. Selecionar todos os personagens
-SELECT * FROM Personagem;
+SELECT * FROM Personagens;
 
--- 7. Selecionar todos as classes
-SELECT * FROM Classe;
+SELECT * FROM Classes;
 
--- 8. Selecionar somente o nome das classes
-SELECT Nome_da_Classe FROM Classe;
+SELECT Nome_da_Classe FROM Classes;
 
--- 9. Selecionar todas as habilidades
-SELECT * FROM Habilidade;
+SELECT * FROM Habilidades;
 
--- 10. Realizar a contagem de quantas habilidades estão cadastradas
-SELECT COUNT(idHabilidade) AS Numero_de_Habilidades_Cadastradas FROM Habilidade;
-
--- 11. Selecionar somente os id’s das habilidades classificando-os por ordem crescente
-SELECT idHabilidade FROM Habilidade
-ORDER BY idHabilidade ASC;
-
--- 12. Selecionar todos os tipos de habilidades
 SELECT * FROM Tipo_de_Habilidade;
 
--- 13. Selecionar todas as habilidades e a quais tipos de habilidades elas fazem parte
-SELECT Habilidade.Nome_Habilidade, Tipo_de_Habilidade.Tipo_de_Habilidade FROM Habilidade
+-- Realizar a contagem de quantas habilidades estão cadastradas
+SELECT COUNT(idHabilidade) AS Numero_de_Habilidades_Cadastradas FROM Habilidades;
+
+-- Selecionar somente os id’s das habilidades classificando-os por ordem crescente
+SELECT idHabilidade FROM Habilidades
+ORDER BY idHabilidade ASC;
+
+
+-- Selecionar todas as habilidades e a quais tipos de habilidades elas fazem parte
+SELECT Habilidades.Nome_Habilidade, Tipo_de_Habilidade.Tipo_de_Habilidade 
+FROM Habilidades
 INNER JOIN Tipo_de_Habilidade
-ON Habilidade.idTipoHabilidade = Tipo_de_Habilidade.idTipoHabilidade;
+ON Habilidades.idTipoHabilidade = Tipo_de_Habilidade.idTipoHabilidade;
 
--- 14. Selecionar todos os personagens e suas respectivas classes
-SELECT Personagem.Nome_do_Personagem, Classe.Nome_da_Classe FROM Personagem
-INNER JOIN Classe
-ON Personagem.idClasse = Classe.idClasse;
+-- Selecionar todos os personagens e suas respectivas classes
+SELECT Personagens.Nome_do_Personagem, Classes.Nome_da_Classe 
+FROM Personagens
+INNER JOIN Classes
+ON Personagens.idClasse = Classes.idClasse;
 
--- 15. Selecionar todos os personagens e as classes (mesmo que elas não tenham correspondência em personagens)
-SELECT Personagem.Nome_do_Personagem, Classe.Nome_da_Classe FROM Personagem
-RIGHT JOIN Classe
-ON Classe.idClasse = Personagem.idClasse
-ORDER BY Personagem.Nome_do_Personagem DESC;
+-- Selecionar todos os personagens e as classes 
+-- (mesmo que elas não tenham correspondência em personagens)
+SELECT Personagens.Nome_do_Personagem, Classes.Nome_da_Classe 
+FROM Personagens
+RIGHT JOIN Classes
+ON Classes.idClasse = Personagens.idClasse
+ORDER BY Personagens.Nome_do_Personagem DESC;
 
--- 16. Selecionar todas as classes e suas respectivas habilidades
-SELECT Classe.Nome_da_Classe, Habilidade.Nome_Habilidade FROM Classe
+-- Selecionar todas as classes e suas respectivas habilidades
+SELECT Classes.Nome_da_Classe, Habilidades.Nome_Habilidade 
+FROM Classes
 LEFT JOIN Habilidades_da_Classe
-ON Classe.idClasse = Habilidades_da_Classe.idClasse
-LEFT JOIN Habilidade
-ON Habilidades_da_Classe.idHabilidade = Habilidade.idHabilidade;
+ON Classes.idClasse = Habilidades_da_Classe.idClasse
+LEFT JOIN Habilidades
+ON Habilidades_da_Classe.idHabilidade = Habilidades.idHabilidade;
 
--- 17. Selecionar todas as habilidades e suas classes (somente as que possuem correspondência)
-SELECT Habilidade.Nome_Habilidade, Classe.Nome_da_Classe FROM Habilidade
+-- Selecionar todas as habilidades e suas classes 
+-- (somente as que possuem correspondência)
+SELECT Habilidades.Nome_Habilidade, Classes.Nome_da_Classe 
+FROM Habilidades
 LEFT JOIN Habilidades_da_Classe
-ON Habilidade.idHabilidade = Habilidades_da_Classe.idHabilidade
-LEFT JOIN Classe
-ON Habilidades_da_Classe.idClasse = Classe.idClasse;
+ON Habilidades.idHabilidade = Habilidades_da_Classe.idHabilidade
+LEFT JOIN Classes
+ON Habilidades_da_Classe.idClasse = Classes.idClasse;
 
--- 18. Selecionar todas as habilidades e suas classes (mesmo que elas não tenham correspondência)
-SELECT Habilidade.Nome_Habilidade, Classe.Nome_da_Classe FROM Habilidade
+-- Selecionar todas as habilidades e suas classes 
+-- (mesmo que elas não tenham correspondência)
+SELECT Habilidades.Nome_Habilidade, Classes.Nome_da_Classe 
+FROM Habilidades
 RIGHT JOIN Habilidades_da_Classe
-ON Habilidade.idHabilidade = Habilidades_da_Classe.idHabilidade
-RIGHT JOIN Classe
-ON Habilidades_da_Classe.idClasse = Classe.idClasse
+ON Habilidades.idHabilidade = Habilidades_da_Classe.idHabilidade
+RIGHT JOIN Classes
+ON Habilidades_da_Classe.idClasse = Classes.idClasse
 ORDER BY Nome_Habilidade DESC;
